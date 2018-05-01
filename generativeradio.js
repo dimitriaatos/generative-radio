5aa02efb3ab9c636b2c389716e41a0911ac3a29c
Dimitri Aatos Ellinas <d.a.ellinas@gmail.com>
2018-05-01 17:20:59 +0300

    svg animation and promises
    A new animated svg added and all callbacks changed to promises.

--- generativeradio.js
+++ generativeradio.js
@@ -1,110 +1,120 @@
 freesound.setToken(freesoundToken);
-// var pieces;
-loadJSON('piece.json');
-var first = true;
+let first = true;
 
 function loadJSON(filename) {
-    //formatting piece object so freesound can read it as a request
-    function formatting(piecesList) {
-        for (let j = 0; j < piecesList.length; j++) {
-            var piece = piecesList[j];
-            for (let i = 0; i < piece.elements.length; i++) {
-                for (let key in piece.elements[i].search.options) {
-                    if (piece.elements[i].search.options[key] == null){
-                        delete options[key];
-                    }
-                }
-                piece.elements[i].search.options.fields = 'username,name,duration,created,url,lisence,previews'
-                if ('filter' in piece.elements[i].search.options) {
-                    var filterString = new String;
-                    for (let key in piece.elements[i].search.options.filter) {
-                        filterString += key+':'+piece.elements[i].search.options.filter[key]+' ';
-                    }
-                    piece.elements[i].search.options.filter = filterString;
-                }
-                piecesList[j] = piece;
-            }
-        }
-        return piecesList;
-    }
+  return new Promise(function(resolve) {
     //XMLHttpRequest requesting JSON file
     var request = new XMLHttpRequest();
     request.addEventListener(
-        'load',
-        function () {
-            var pieces = formatting(JSON.parse(this.responseText));
-            loadSounds(pieces);
-        }
+      'load',
+      function() {
+        resolve(JSON.parse(this.responseText));
+      }
     );
     request.open('GET', filename);
     request.send();
+  })
 }
-//calls loadSounds
 
-function loadSounds(pieces){
-    var random = new noRep(pieces.length, 1, 1);
-
-    ! function choosePiece() {
-        var currentPieceIndex = random.next();
-        var piece = pieces[currentPieceIndex];
-        var count = 0;
-        //requesting sounds, triggers collect function
-        for (let i = 0; i < piece.elements.length; i++) {
-            if (typeof piece.elements[i].sounds == 'undefined') {
-                freesound.textSearch(
-                    piece.elements[i].search.text,
-                    piece.elements[i].search.options,
-                    function(response){
-                        piece.elements[i].sounds = response.results;
-                        pieces[currentPieceIndex].piece;
-                        counter();
-                    },
-                    function(){alert('Error while searching...');}
-                )
-            }
-            else {counter()}
+function reformatting(pieces) {
+  for (let j = 0; j < pieces.length; j++) {
+    var piece = pieces[j];
+    for (let i = 0; i < piece.elements.length; i++) {
+      for (let key in piece.elements[i].search.options) {
+        if (piece.elements[i].search.options[key] == null) {
+          delete options[key];
         }
-        //
-        function counter() {
-            count++;
-            if (count == piece.elements.length) {
-                //start
-                var play = new playPiece(piece);
-                //stop
-                setTimeout(
-                    function () {
-                        play.stop();
-                        choosePiece();
-                    },
-                    piece.duration
-                );
-            }
+      }
+      piece.elements[i].search.options.fields = 'username,name,duration,created,url,lisence,previews'
+      if ('filter' in piece.elements[i].search.options) {
+        var filterString = new String;
+        for (let key in piece.elements[i].search.options.filter) {
+          filterString += key + ':' + piece.elements[i].search.options.filter[key] + ' ';
         }
-    }()
+        piece.elements[i].search.options.filter = filterString;
+      }
+      pieces[j] = piece;
+    }
+  }
+  return pieces;
 }
-//calls playPiece
+
+function loadPiece(args) {
+  return new Promise(function(resolve, reject) {
+    let piece = args.pieces[args.index];
+    if (!piece.loaded) {
+      let remainingRequests = piece.elements.length;
+      for (let i = 0; i < piece.elements.length; i++) {
+        freesound.textSearch(
+          piece.elements[i].search.text,
+          piece.elements[i].search.options,
+          function(response) {
+            piece.elements[i].sounds = response.results;
+            --remainingRequests;
+            if (remainingRequests <= 0) {
+              args.pieces[args.index] = piece;
+              args.pieces[args.index].loaded = true;
+              resolve({
+                pieces: args.pieces,
+                index: args.index
+              });
+            }
+          },
+          function() {
+            reject()
+          }
+        )
+      }
+    } else {
+      resolve({
+        pieces: args.pieces,
+        index: args.index
+      });
+    }
+  });
+}
+
+function loadAndPlay(args) {
+  return new Promise(function (resolve) {
+    loadPiece(args).then(function (response) {
+      playPiece(response.pieces[response.index]).then(resolve)
+    })
+  });
+}
+
+function play(filename) {
+  var norep;
+  loadJSON(filename).then(function(response) {
+    let pieces = reformatting(response);
+    norep = new noRep(pieces.length, 1, 1);
+    (function sequence(pieces) {
+      new Promise(function (resolve) {
+        loadAndPlay({pieces:pieces, index:norep.next()}).then(resolve);
+      }).then(sequence.bind(this, pieces))
+    })(pieces);
+  })
+}
 
 window.AudioContext = window.AudioContext || window.webkitAudioContext;
-  context = new AudioContext();
+context = new AudioContext();
 
 function buffer(link) {
   // Fix up prefixing
   var bufferLoader = new BufferLoader(
-    context,
-    [
+    context, [
       link
     ],
     finishedLoading
-    );
+  );
 
   bufferLoader.load();
 }
 
 function finishedLoading(file) {
   if (first) {
-          mess();
-          first = false;
-      }
+    loaded();
+    first = false;
+  }
   var source = context.createBufferSource();
   source.buffer = file[0];
 
