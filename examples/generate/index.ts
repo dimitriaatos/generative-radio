8e5ac07940220e733b0550bb1b7473f4ccae1777
Dimitri Aatos Ellinas <d.a.ellinas@gmail.com>
2024-02-01 22:29:43 +0200

    feat: generate mp3

--- examples/generate/index.ts
+++ examples/generate/index.ts
@@ -1,12 +1,12 @@
 import toWav from 'audiobuffer-to-wav'
 import 'dotenv/config'
 import { default as FreeSound } from 'freesound-client'
-import fs from 'fs'
 import { OfflineAudioContext } from 'node-web-audio-api'
 import { Buffer } from 'node:buffer'
 import createGenerativeRadio from '../../src/index.js'
 import { Pieces } from '../../src/types/Pieces.js'
 import pieces from '../pieces.json' assert { type: 'json' }
+import { Lame } from 'node-lame'
 
 const sampleRate = 44100
 const channels = 2
@@ -34,6 +34,18 @@ await createGenerativeRadio({
 console.log('- Loaded, start recording...')
 const recording = await offlineContext.startRendering()
 
-console.log('- Save to a file (generative.wav)')
+console.log('- Write to disc (generative.mp3)')
 const buffer = Buffer.from(new Uint8Array(toWav(recording)))
-fs.writeFileSync('generative.wav', buffer)
+const encoder = new Lame({
+	output: './generative.mp3',
+	bitrate: 128,
+}).setBuffer(buffer)
+
+encoder
+	.encode()
+	.then(() => {
+		console.log('Encoding finished')
+	})
+	.catch((error) => {
+		console.error(error)
+	})
